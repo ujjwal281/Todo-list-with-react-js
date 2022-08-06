3975ae9ad66e187da8a142f8c71319efaa8f4241
Ujjwal <negiujjwal78@gmail.com>
2022-08-06 19:36:46 +0530

    Initial commit

---
+++ precache-manifest.cc44236525ee0bc85b5472083e4dfb07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3b859ab5b16e6ffad2d",
+    "url": "/static/css/main.2b20e31b.chunk.css"
+  },
+  {
+    "revision": "c3b859ab5b16e6ffad2d",
+    "url": "/static/js/main.c3b859ab.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "/static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "f2f9d634cd238ef549fb",
+    "url": "/static/js/2.f2f9d634.chunk.js"
+  },
+  {
+    "revision": "526e76a96793b81fba07",
+    "url": "/static/js/runtime~main.526e76a9.js"
+  },
+  {
+    "revision": "3b982751b3a8e68a88613ee69cf15056",
+    "url": "/index.html"
+  }
+];
